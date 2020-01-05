816a1c63a3e471ead97c5ec475a2649d16998afc
Shubham Gupta <shubham_gupta5@dell.com>
2020-01-06 00:42:57 +0530

    Push Debug Folder

---
+++ Debug/RAP_Change.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Surya
+{
+    partial class rapchange
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.metroGrid1 = new MetroFramework.Controls.MetroGrid();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SrNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Wt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Color = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Purity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Shape = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CPS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CPC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RAP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Disc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Amt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.metroGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroGrid1
+            // 
+            this.metroGrid1.AllowUserToAddRows = false;
+            this.metroGrid1.AllowUserToOrderColumns = true;
+            this.metroGrid1.AllowUserToResizeColumns = false;
+            this.metroGrid1.AllowUserToResizeRows = false;
+            this.metroGrid1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.metroGrid1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.metroGrid1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.metroGrid1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.metroGrid1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.metroGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.metroGrid1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SrNo,
+            this.id,
+            this.Wt,
+            this.Color,
+            this.Purity,
+            this.Shape,
+            this.CPS,
+            this.CPC,
+            this.RAP,
+            this.Disc,
+            this.Rate,
+            this.Amt});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.metroGrid1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.metroGrid1.EnableHeadersVisualStyles = false;
+            this.metroGrid1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.metroGrid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.metroGrid1.Location = new System.Drawing.Point(23, 63);
+            this.metroGrid1.Name = "metroGrid1";
+            this.metroGrid1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(174)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(198)))), ((int)(((byte)(247)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.metroGrid1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.metroGrid1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.metroGrid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.metroGrid1.Size = new System.Drawing.Size(1001, 430);
+            this.metroGrid1.TabIndex = 0;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Highlight = true;
+            this.metroButton1.Location = new System.Drawing.Point(949, 499);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Red;
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "RESET";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.UseStyleColors = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Highlight = true;
+            this.metroButton2.Location = new System.Drawing.Point(868, 499);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(75, 23);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroButton2.TabIndex = 2;
+            this.metroButton2.Text = "UPDATE";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.UseStyleColors = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // SrNo
+            // 
+            this.SrNo.DividerWidth = 1;
+            this.SrNo.HeaderText = "Sr. No";
+            this.SrNo.Name = "SrNo";
+            this.SrNo.ReadOnly = true;
+            this.SrNo.Width = 40;
+            // 
+            // id
+            // 
+            this.id.DividerWidth = 1;
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Width = 60;
+            // 
+            // Wt
+            // 
+            this.Wt.DividerWidth = 1;
+            this.Wt.HeaderText = "Weight";
+            this.Wt.Name = "Wt";
+            this.Wt.Width = 60;
+            // 
+            // Color
+            // 
+            this.Color.DividerWidth = 1;
+            this.Color.HeaderText = "Color";
+            this.Color.Name = "Color";
+            this.Color.Width = 80;
+            // 
+            // Purity
+            // 
+            this.Purity.DividerWidth = 1;
+            this.Purity.HeaderText = "Purity";
+            this.Purity.Name = "Purity";
+            this.Purity.Width = 80;
+            // 
+            // Shape
+            // 
+            this.Shape.DividerWidth = 1;
+            this.Shape.HeaderText = "Shape";
+            this.Shape.Name = "Shape";
+            // 
+            // CPS
+            // 
+            this.CPS.DividerWidth = 1;
+            this.CPS.HeaderText = "CPS";
+            this.CPS.Name = "CPS";
+            // 
+            // CPC
+            // 
+            this.CPC.DividerWidth = 1;
+            this.CPC.FillWeight = 90F;
+            this.CPC.HeaderText = "CPC";
+            this.CPC.Name = "CPC";
+            this.CPC.Width = 90;
+            // 
+            // RAP
+            // 
+            this.RAP.DividerWidth = 1;
+            this.RAP.HeaderText = "RAP";
+            this.RAP.Name = "RAP";
+            this.RAP.Width = 80;
+            // 
+            // Disc
+            // 
+            this.Disc.DividerWidth = 1;
+            this.Disc.HeaderText = "Disc";
+            this.Disc.Name = "Disc";
+            this.Disc.Width = 70;
+            // 
+            // Rate
+            // 
+            this.Rate.DividerWidth = 1;
+            this.Rate.HeaderText = "Rate";
+            this.Rate.Name = "Rate";
+            // 
+            // Amt
+            // 
+            this.Amt.DividerWidth = 1;
+            this.Amt.HeaderText = "Amount";
+            this.Amt.Name = "Amt";
+            // 
+            // rapchange
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 540);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroGrid1);
+            this.Name = "rapchange";
+            this.Text = "Change RAP and Discount";
+            this.Load += new System.EventHandler(this.RAP_Change_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.metroGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroGrid metroGrid1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SrNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Wt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Color;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Purity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Shape;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CPS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CPC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RAP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Disc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Amt;
+    }
+}
---
+++ Debug/RAP_Change.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using MetroFramework;
+using MySql.Data.MySqlClient;
+
+
+namespace Surya
+{
+    public partial class rapchange : MetroForm
+    {
+        public rapchange()
+        {
+            InitializeComponent();
+        }
+
+        private void RAP_Change_Load(object sender, EventArgs e)
+        {
+            metroButton1.PerformClick();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            metroGrid1.Rows.Clear();
+            metroGrid1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            metroGrid1.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            String q1 = "Select id,wt,color,purity,shape,cps,cpc,rap,disc,rate,amt from invent where status!='S' order by date_pur;";
+
+            foreach (DataGridViewColumn col in metroGrid1.Columns)
+            {
+                //col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                col.DefaultCellStyle.Font = new System.Drawing.Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Pixel);
+            }
+            int row = 0;
+            int count = 1;
+
+            MySqlConnection con1 = new MySqlConnection("SERVER=localhost;DATABASE=Sales;UID=root;PASSWORD=smhs; Connection Timeout=30; Min Pool Size=20; Max Pool Size=300;");
+            MySqlCommand cmd1 = new MySqlCommand(q1, con1);
+            MySqlDataReader dataReader1;
+            con1.Open();
+            dataReader1 = cmd1.ExecuteReader();
+            int index = 0;
+            while (dataReader1.Read())
+            {
+                if (index != 0)
+                    index = this.metroGrid1.Rows.Count;
+
+                index++;
+
+                this.metroGrid1.Rows.Add();
+
+                (metroGrid1.Rows[row].Cells[0]).Value = count;
+                (metroGrid1.Rows[row].Cells[1]).Value = dataReader1.GetValue(0);
+                (metroGrid1.Rows[row].Cells[2]).Value = dataReader1.GetValue(1);
+                (metroGrid1.Rows[row].Cells[3]).Value = dataReader1.GetValue(2);
+                (metroGrid1.Rows[row].Cells[4]).Value = dataReader1.GetValue(3);
+                (metroGrid1.Rows[row].Cells[5]).Value = dataReader1.GetValue(4);
+                (metroGrid1.Rows[row].Cells[6]).Value = dataReader1.GetValue(5);
+                (metroGrid1.Rows[row].Cells[7]).Value = dataReader1.GetValue(6);
+                (metroGrid1.Rows[row].Cells[8]).Value = Convert.ToInt32(dataReader1.GetValue(7));
+                (metroGrid1.Rows[row].Cells[9]).Value = dataReader1.GetValue(8);
+                (metroGrid1.Rows[row].Cells[10]).Value = dataReader1.GetValue(9);
+                (metroGrid1.Rows[row].Cells[11]).Value = dataReader1.GetValue(10);
+
+                count++;
+                row++;
+            }
+                                
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            string id = "",color="",purity="",shape="",cps="";
+            double wt = 0, rap = 0, disc = 0,disc1=0, dol = 0, rate = 0, amt = 0;
+            MySqlConnection con1 = new MySqlConnection("SERVER=localhost;DATABASE=sales;UID=root;PASSWORD=smhs;");
+            MySqlConnection con2 = new MySqlConnection("SERVER=localhost;DATABASE=sales;UID=root;PASSWORD=smhs;");
+            con1.Open();
+            String q1 = "Select dollar from glob;";
+            MySqlCommand cmd1 = new MySqlCommand(q1, con1);
+            MySqlDataReader dr = cmd1.ExecuteReader();
+            while (dr.Read())
+            {
+                dol = Convert.ToDouble(dr.GetValue(0));
+            }
+            dr.Close();
+            for (int rows = 0; rows < metroGrid1.Rows.Count; rows++)
+            {
+                id = metroGrid1.Rows[rows].Cells[1].Value.ToString();
+                color = metroGrid1.Rows[rows].Cells[3].Value.ToString();
+                purity = metroGrid1.Rows[rows].Cells[4].Value.ToString();
+                shape = metroGrid1.Rows[rows].Cells[5].Value.ToString();
+                cps = metroGrid1.Rows[rows].Cells[6].Value.ToString();
+                wt = Convert.ToDouble(metroGrid1.Rows[rows].Cells[2].Value.ToString());
+                rap = Convert.ToDouble(metroGrid1.Rows[rows].Cells[7].Value.ToString());
+                disc = Convert.ToDouble(metroGrid1.Rows[rows].Cells[8].Value.ToString());
+                rate = Convert.ToDouble(metroGrid1.Rows[rows].Cells[9].Value.ToString());
+                amt = Convert.ToDouble(metroGrid1.Rows[rows].Cells[10].Value.ToString());
+
+                disc1 = (100 - disc) * .01;
+                rate = rap * dol * disc1;
+                amt = rate * wt;
+                con2.Open();
+                String q2 = "Update invent set rate=" + rate + ", amt=" + amt + ", rap="+rap+", disc="+disc+", wt="+wt+", color='"+color+"', purity='"+purity+"', shape='"+shape+"', cps='"+cps+"' where id='" + id + "';";
+                MySqlCommand cmd2 = new MySqlCommand(q2, con2);
+                cmd2.ExecuteNonQuery();
+                con2.Close();
+            }
+            MetroMessageBox.Show(this, "DATABASE IS UPDATED!!", "UPDATION", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            metroButton1.PerformClick();
+            Application.OpenForms["Home"].BringToFront();
+            Application.OpenForms["RAP_Change"].BringToFront();
+
+        }
+    }
+}
